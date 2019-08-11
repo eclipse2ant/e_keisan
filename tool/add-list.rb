c7808e9fdb4ddefeab410d5b4c0a331e4d0a3e41
eclipse2ant <eclipse2ant@gmail.com>
2019-08-12 01:35:19 +0900

    writing add-list.rb

--- tool/add-list.rb
+++ tool/add-list.rb
@@ -10,5 +10,28 @@ ARGV.each do |f|
 	data = read_file(f)
 	d<< data
 end
-p d[1][5]
+for j in 0...d[1].length-1 do
+	unless d[1][j][0]==""
+		puts d[1][j].join(',')
+		j++
+		next
+	else
+		for i in 0...d[0].length-1 do
+			unless d[0][i][1]==d[1][j][4]
+				i++
+				next
+			elsif d[0][i][0]==""
+				i++
+				next
+			else
+				holder=[]
+				for k in 0...d[0].length-(i+1) do
+					holder<<d[0][i+k][1]
+				end
+				unless holder.include? d[1][j]
+					puts d[0][	
+		end
+	end
+end	
+
 
