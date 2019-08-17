62c927b2f70049386fd930be554241e251f5e90b
eclipse2ant <eclipse2ant@gmail.com>
2019-08-17 16:23:51 +0900

    grep.rb

--- grep.rb
+++ grep.rb
@@ -1,23 +1,41 @@
 #!/usr/local/bin/ruby -Ku
 # conding utf-8
 
-def check_usage   
-  if ARGV.length == 0 
-    puts "Usage: #{File.basename($0)}  keyword  filename"
-    exit
-  end
+require 'optparse'
+
+class GrepArguments < Hash
+	def initialize(args)
+		super()
+		opts = OptionParser.new do |opts|
+			opts.banner = "Usage: #$0 [options]"
+			opts.on('-n') do 
+				self[:number] = true
+			end
+			opts.on_tail('-h', '--help', 'display this help and exit') do
+				puts opts
+				exit
+			end
+		end
+    begin
+      opts.parse!(args)
+    rescue
+      $stderr.puts  $!
+    end
+	end
 end
 
+args = GrepArguments.new(ARGV)
 
-check_usage
 str=ARGV.shift
 reg=Regexp.new(str)
 begin
 	ARGF.each do |line|
-#  	if line =~ /#{str}/
   	if reg =~ line
-#   	print "#{ARGF.filename} #{ARGF.file.lineno}: #{line}"
+			if args[:number]
+   			print "#{ARGF.filename} #{ARGF.file.lineno}: #{line}"
+			else
     	print "#{line}"
+			end
   	end
 	end
 rescue
