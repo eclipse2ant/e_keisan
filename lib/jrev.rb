4fa2e7b68122623246c7a8bd886ad51c7166cb83
eclipse2ant <eclipse2ant@gmail.com>
2019-09-11 15:33:00 +0900

    jrev.rb 3

--- lib/jrev.rb
+++ lib/jrev.rb
@@ -11,16 +11,49 @@ require 'jrev_util'
 
 include RevUtil
 
-p ARGV[0]
+p ARGV[1]
 
+
+
+
+
+list=[]
 begin
   ifile = get_filename(ARGV[0])
   wb=get_workbook(ifile)
-  sh=SheetHolder.instance.sheet
+#  sh=SheetHolder.instance.sheet
   sh=get_sheet(wb)
-  sh[2,6]="501"
-  p sh[2,6]
-  ofile=put_filename(ARGV[0])
+  i=0
+  for i in 6..58 do
+    list<< [sh[i,1],sh[i,5]]
+  end
+  for i in 67..2240 do
+    list<< [sh[i,1],sh[i,5]]
+  end
+  p list[0][1]
+ensure
+  ifile.close
+#  ofile.close
+end
+
+begin
+  ifile = get_filename(ARGV[1])
+  wb=get_workbook(ifile)
+#  sh=SheetHolder.instance.sheet
+  sh=get_sheet(wb)
+#  p sh[7,50]
+#  sh[9,51]=list[0][1]
+#  p sh[9,51]
+  list.each do |d|
+    j=0
+    for j in 9..2199 do
+      if sh[j,2]==d[0]
+        sh[j,51]=d[1]
+        break
+      end
+    end
+  end
+  ofile=put_filename(ARGV[1])
   wb.write(ofile)
 ensure
   ifile.close
